a21cf4f445ff4d0b6e5fd07856c62f3d74650a1d
eager-class-5814 <candacehchoe@gmail.com>
2020-06-08 01:26:56 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,4 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  include Dance #includes Dance module 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
 end 
